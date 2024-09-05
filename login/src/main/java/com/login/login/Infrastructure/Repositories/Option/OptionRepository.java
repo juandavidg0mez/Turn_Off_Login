dfed96bb5444a92306656b08e785c45de9c08e0d
juandavidg0mez <nihlgomez@gamil.com>
2024-09-05 02:21:35 -0500

    Push Correcion Errores

--- login/src/main/java/com/login/login/Infrastructure/Repositories/Option/OptionRepository.java
+++ login/src/main/java/com/login/login/Infrastructure/Repositories/Option/OptionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.login.login.Domain.Option;
 
 public interface OptionRepository  extends JpaRepository<Option, Long>{
-
+    
 }
